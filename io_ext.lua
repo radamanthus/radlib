0c079dfb56ceca8dd3898dd5991e99584f14bd0a
Radamanthus Batnag <rad@batnag.org>
2013-08-24 15:26:33 +0800

    Add copyFile function

--- io_ext.lua
+++ io_ext.lua
@@ -3,10 +3,47 @@ local ltn12 = require("ltn12")
 
 local M = {}
 
+-- From http://docs.coronalabs.com/api/type/File/write.html
+local copyFile = function( srcName, srcPath, dstName, dstPath, overwrite )
+  local results = true                -- assume no errors
+
+  -- Copy the source file to the destination file
+  local rfilePath = system.pathForFile( srcName, srcPath )
+  local wfilePath = system.pathForFile( dstName, dstPath )
+
+  local rfh = io.open( rfilePath, "rb" )
+  local wfh = io.open( wfilePath, "wb" )
+
+  if  not wfh then
+    print( "writeFileName open error!" )
+    results = false -- error
+  else
+    -- Read the file from the Resource directory and write it to the destination directory
+    local data = rfh:read( "*a" )
+
+    if not data then
+      print( "read error!" )
+      results = false     -- error
+    else
+      if not wfh:write( data ) then
+        print( "write error!" )
+        results = false -- error
+      end
+    end
+  end
+
+  -- Clean up our file handles
+  rfh:close()
+  wfh:close()
+
+  return results
+end
+M.copyFile = copyFile
+
 -- Taken from http://stackoverflow.com/questions/4990990/lua-check-if-a-file-exists
 local fileExists = function( filename )
-   local f = io.open( filename, "r" )
-   if f ~= nil then io.close( f ) return true else return false end
+  local f = io.open( filename, "r" )
+  if f ~= nil then io.close( f ) return true else return false end
 end
 M.fileExists = fileExists
 
